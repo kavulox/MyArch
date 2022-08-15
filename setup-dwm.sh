61693b6e3639152cceca90525c9d34da2c818716
kavulox <108770990+kavulox@users.noreply.github.com>
2022-08-15 15:39:14 +0000

    Update setup-dwm.sh

--- setup-dwm.sh
+++ setup-dwm.sh
@@ -1,13 +1,13 @@
-echo -e "d\nd\nd\nd\n\n\nw" | fdisk /dev/vda
-echo -e "n\np\n1\n\n+1G\nw" | fdisk /dev/vda
-echo -e "n\np\n2\n\n\nw" | fdisk /dev/vda
-mkfs.fat -F32 /dev/vda1
-mkfs.ext4 /dev/vda2
-mount /dev/vda2 /mnt
+echo -e "d\nd\nd\nd\n\n\nw" | fdisk /dev/sda
+echo -e "n\np\n1\n\n+1G\nw" | fdisk /dev/sda
+echo -e "n\np\n2\n\n\nw" | fdisk /dev/sda
+mkfs.fat -F32 /dev/sda
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
 pacstrap /mnt base linux linux-firmware nano neofetch
 genfstab -U /mnt >> /mnt/etc/fstab
 cat << EOF | arch-chroot /mnt
-timedatectl set-timezone Canada/Central
+timedatectl set-timezone Canada/Eastern
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
@@ -15,32 +15,28 @@ echo aslen-pc > /etc/hostname
 touch /etc/hosts
 echo "127.0.0.1     localhost" > /etc/hosts
 echo "::1           localhost" >> /etc/hosts
-echo "127.0.1.1     aslen-pc" >> /etc/hosts
-echo -e "test\ntest" | passwd root
+echo "127.0.1.1     josh-pc" >> /etc/hosts
+echo -e "josh\njosh" | passwd root
 yes | pacman -S grub efibootmgr
 mkdir /boot/efi
-mount /dev/vda1 /boot/efi
+mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
-useradd -m aslen
-echo -e "test\ntest" | passwd aslen
+useradd -m josh
+echo -e "josh\njosh" | passwd josh
 yes | pacman -S sudo nano
-sed -i '80i aslen ALL=(ALL) ALL' /etc/sudoers
+sed -i '80i josh ALL=(ALL) ALL' /etc/sudoers
 pacman -S --noconfirm xorg-xinit xorg git base-devel networkmanager
 cd /usr/src
-git clone git://git.suckless.org/dwm
-git clone git://git.suckless.org/st
+git clone github.com/kavulox/dwm
 git clone git://git.suckless.org/dmenu
 systemctl enable NetworkManager.service
 cd dwm
 make clean install
 cd ..
-cd st
-make clean install
-cd ..
 cd dmenu
 make clean install
 cd ..
-pacman -Syy
-echo 'Login as a user, then type 'nano ~/.xinitrc' and add "exec dwm" to it'
+pacman -Syy alacritty feh firefox 
+echo "exec dwm" > /home/josh/.xinitrc 
 EOF
